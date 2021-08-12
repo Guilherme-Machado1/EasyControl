9863d2753e07f921bbdddffa658a94dc213cad4a
Guilherme Santos <guilhermemachado1900@gmail.com>
2021-08-12 13:46:54 -0300

    Login e logout

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,3 +1,5 @@
+const session = require('express-session');
+const Home = require('../models/HomeModel');
 exports.paginaInicial = (req, res) => {
   res.render('index', {
     titulo: 'Este será o título da página',
@@ -6,7 +8,42 @@ exports.paginaInicial = (req, res) => {
   return;
 };
 
-exports.trataPost = (req, res) => {
-  res.render('telaInicial');
-  return;
+exports.login = async (req, res) => {
+  try {
+    const login = new Home(req.body);
+    await login.login();
+
+    if(login.errors.length > 0){
+      req.flash('errors', login.errors);
+      req.session.save(function(){
+      return res.redirect('back');
+      });
+    }
+  
+    if(!login.user){
+      req.flash('errors', 'algo deu errado');
+      req.session.save(function(){
+      return res.redirect('back');
+      });
+    }
+  
+
+    req.flash('success', 'O login foi feito com sucesso');
+    req.session.user = login.user;
+    req.session.save(function(){
+      return res.render('telaInicial');
+    });
+  
+    
+  }catch (error) {
+    console.log(error);
+    res.redirect("404");
+  }
+    
+  
+}
+
+exports.logoff = (req, res) => {
+  req.session.destroy();
+  return res.redirect('/');
 };
