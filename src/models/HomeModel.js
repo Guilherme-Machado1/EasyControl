5d271c70d2f70dc6c1204d14633116c7f0ecdf0b
Guilherme Santos <guilhermemachado1900@gmail.com>
2021-08-12 14:09:48 -0300

    Conexão com banco de dados e esquema do DB

--- src/models/HomeModel.js
+++ src/models/HomeModel.js
@@ -1,13 +1,44 @@
 const mongoose = require('mongoose');
 
 const HomeSchema = new mongoose.Schema({
-  titulo: { type: String, required: true },
-  descricao: String
+  usuario: { type: String, required: true },
+  senha: { type: String, required: true }
 });
 
 const HomeModel = mongoose.model('Home', HomeSchema);
 
 class Home {
+    constructor(body){
+      this.body = body;
+      this.errors = [];
+      this.user = null;
+    }
+
+     async login(){
+      this.cleanUp();
+      if(this.errors.length > 0) return;
+        this.user = await HomeModel.findOne({senha: this.body.senha, usuario: this.body.usuario});
+        if(this.user === null){
+          this.errors.push('Senha ou usuário incorretas');
+          console.log(this.user);
+          return;
+        }
+        console.log(this.user);
+    }
+
+
+    cleanUp(){
+      for(let key in this.body){
+        if(typeof key !== 'string'){
+          this.body[key] = '';
+        }
+      }
+
+      this.body = {
+        usuario: this.body.usuario,
+        senha: this.body.senha
+      }
+    }
 
 }
 
